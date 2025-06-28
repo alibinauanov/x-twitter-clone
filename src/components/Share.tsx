bc64f49986350cfee22d8c911783e652e91925ec
alibinauanov <an3502@nyu.edu>
2025-06-28 16:54:29 +0500

    feed added

---
+++ src/components/Share.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React, { useState } from "react";
+import Image from "./Image";
+import { shareAction } from "@/action";
+
+const Share = () => {
+    const [media, setMedia] = useState<File | null>(null);
+
+    const handleMediaChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files[0]) {
+            setMedia(e.target.files[0]);
+        }
+    };
+
+    return (
+        <form className="p-4 flex gap-4" action={shareAction}>
+            <div className="relative w-10 h-10 rounded-full overflow-hidden">
+                <Image path="general/footballMe.jpg" alt="" w={100} h={100} tr={true} />
+            </div>
+            <div className="flex-1 flex flex-col gap-4">
+                <input type="text" name="desc" placeholder="What is happening?!" className="bg-transparent outline-none placeholder:text-textGray text-xl"/>
+                <div className="flex items-center justify-between gap-4 flex-wrap">
+                    <div className="flex gap-4 flex-wrap">
+                        <input type="file" name="file" onChange={handleMediaChange} className="hidden" id="file" />
+                        <label htmlFor="file">
+                            <Image path="icons/image.svg" alt="" w={20} h={20} className="cursor-pointer" />
+                        </label>
+                        <Image path="icons/gif.svg" alt="" w={20} h={20} className="cursor-pointer" />
+                        <Image path="icons/poll.svg" alt="" w={20} h={20} className="cursor-pointer" />
+                        <Image path="icons/emoji.svg" alt="" w={20} h={20} className="cursor-pointer" />
+                        <Image path="icons/schedule.svg" alt="" w={20} h={20} className="cursor-pointer" />
+                        <Image path="icons/location.svg" alt="" w={20} h={20} className="cursor-pointer" />
+                    </div>
+                    <button className="bg-white text-black font-bold rounded-full py-2 px-4">Post</button>
+                </div>
+            </div>
+        </form>
+    );
+};
+
+export default Share;
